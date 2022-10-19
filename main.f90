96ef65d7a702afe141d49dd4a52e443fc7cca54c
Dan Barr <dan.barr@outlook.com>
2022-10-19 17:24:35 -0400

    feedline - all one word

--- main.f90
+++ main.f90
@@ -14,7 +14,7 @@ program rfe
     end type
 
     integer :: xmtr_power = 1000
-    integer :: feed_line_length = 73
+    integer :: feedline_length = 73
     real    :: duty_cycle = .5
     real    :: per_30 = .5
     integer :: j 
@@ -38,7 +38,7 @@ program rfe
     do j = 1, size(all_frequency_values)
         current_freq_values = all_frequency_values(j)
         yarg = calculate_uncontrolled_safe_distance( current_freq_values, &
-                    cable_values, xmtr_power, feed_line_length, duty_cycle, per_30 )
+                    cable_values, xmtr_power, feedline_length, duty_cycle, per_30 )
     
         write (*,"(f0.2)") yarg           
     end do
@@ -50,34 +50,34 @@ program rfe
             rval = abs((frequency_values%swr - 1)/(frequency_values%swr + 1))
         end function calculate_reflection_coefficient
 
-        real function calculate_feed_line_loss_for_matched_load_at_frequency(feedline_length, feedline_loss_per_100ft_at_frequency)&
+        real function calculate_feedline_loss_for_matched_load_at_frequency(feedline_length, feedline_loss_per_100ft_at_frequency)&
              result(rval)
             integer, intent(in) :: feedline_length
             real, intent(in)    :: feedline_loss_per_100ft_at_frequency
             rval = (feedline_length/100) * feedline_loss_per_100ft_at_frequency
-        end function calculate_feed_line_loss_for_matched_load_at_frequency
+        end function calculate_feedline_loss_for_matched_load_at_frequency
     
-        real function calculate_feed_line_loss_for_matched_load_at_frequency_pct(feedline_loss_for_matched_load) result (rval)
+        real function calculate_feedline_loss_for_matched_load_at_frequency_pct(feedline_loss_for_matched_load) result (rval)
             real, intent(in) ::  feedline_loss_for_matched_load
             rval = 10**( -feedline_loss_for_matched_load/10)
-        end function calculate_feed_line_loss_for_matched_load_at_frequency_pct
+        end function calculate_feedline_loss_for_matched_load_at_frequency_pct
         
-        real function calculate_feed_line_loss_per_100ft_at_frequency(frequency_values, cable_values) result(rval)
+        real function calculate_feedline_loss_per_100ft_at_frequency(frequency_values, cable_values) result(rval)
             type(t_frequencyvalues), intent(in) :: frequency_values
             type(t_cablevalues), intent(in) :: cable_values
             rval = cable_values%k1 * sqrt(frequency_values%freq + cable_values%k2 * frequency_values%freq)
-        end function calculate_feed_line_loss_per_100ft_at_frequency
+        end function calculate_feedline_loss_per_100ft_at_frequency
 
-        real function calculate_feed_line_loss_for_swr (feedline_loss_for_matched_load_percentage, gamma_squared) result(rval)
+        real function calculate_feedline_loss_for_swr (feedline_loss_for_matched_load_percentage, gamma_squared) result(rval)
             real, intent(in) :: feedline_loss_for_matched_load_percentage, gamma_squared
             rval = -10 * log10(feedline_loss_for_matched_load_percentage * &
                 ((1 - gamma_squared)/(1 - feedline_loss_for_matched_load_percentage**2 * gamma_squared)) )
-        end function calculate_feed_line_loss_for_swr
+        end function calculate_feedline_loss_for_swr
 
-        real function calculate_feed_line_loss_for_swr_percentage(feedline_loss_for_swr) result(rval)
+        real function calculate_feedline_loss_for_swr_percentage(feedline_loss_for_swr) result(rval)
             real feedline_loss_for_swr
             rval = ( 100 - 100/( 10**( feedline_loss_for_swr/10 ) ) )/100
-        end function calculate_feed_line_loss_for_swr_percentage
+        end function calculate_feedline_loss_for_swr_percentage
 
         real function calculate_uncontrolled_safe_distance(freq_values, cable_values, transmitter_power, &
             feedline_length, duty_cycle, uncontrolled_percentage_30_minutes) result(rval)
@@ -89,31 +89,30 @@ program rfe
             real uncontrolled_percentage_30_minutes
 
             real gamma, gamma_squared 
-            real feed_line_loss_per_100ft_at_frequency, feed_line_loss_for_matched_load_at_frequency, & 
-                feed_line_loss_for_matched_load_at_frequency_percentage    
-            real feed_line_loss_for_swr, feed_line_loss_for_swr_percentage, power_loss_at_swr
+            real feedline_loss_per_100ft_at_frequency, feedline_loss_for_matched_load_at_frequency, & 
+                feedline_loss_for_matched_load_at_frequency_percentage    
+            real feedline_loss_for_swr, feedline_loss_for_swr_percentage, power_loss_at_swr
             real uncontrolled_average_pep, peak_envelope_power_at_antenna, gain_decimal, mpe_s
 
             gamma = calculate_reflection_coefficient(freq_values)
 
-            feed_line_loss_per_100ft_at_frequency = & 
-                calculate_feed_line_loss_per_100ft_at_frequency(freq_values, cable_values)
+            feedline_loss_per_100ft_at_frequency = & 
+                calculate_feedline_loss_per_100ft_at_frequency(freq_values, cable_values)
     
-            feed_line_loss_for_matched_load_at_frequency = &
-                calculate_feed_line_loss_for_matched_load_at_frequency(feedline_length, feed_line_loss_per_100ft_at_frequency)
+            feedline_loss_for_matched_load_at_frequency = &
+                calculate_feedline_loss_for_matched_load_at_frequency(feedline_length, feedline_loss_per_100ft_at_frequency)
     
-            feed_line_loss_for_matched_load_at_frequency_percentage = & 
-                calculate_feed_line_loss_for_matched_load_at_frequency_pct(feed_line_loss_for_matched_load_at_frequency)
+            feedline_loss_for_matched_load_at_frequency_percentage = & 
+                calculate_feedline_loss_for_matched_load_at_frequency_pct(feedline_loss_for_matched_load_at_frequency)
     
             gamma_squared = abs(gamma)**2
     
-            feed_line_loss_for_swr = &
-                calculate_feed_line_loss_for_swr(feed_line_loss_for_matched_load_at_frequency_percentage, gamma_squared)
+            feedline_loss_for_swr = &
+                calculate_feedline_loss_for_swr(feedline_loss_for_matched_load_at_frequency_percentage, gamma_squared)
     
-            feed_line_loss_for_swr_percentage = & 
-                calculate_feed_line_loss_for_swr_percentage( feed_line_loss_for_swr )
+            feedline_loss_for_swr_percentage = calculate_feedline_loss_for_swr_percentage( feedline_loss_for_swr )
     
-            power_loss_at_swr = feed_line_loss_for_swr_percentage * transmitter_power
+            power_loss_at_swr = feedline_loss_for_swr_percentage * transmitter_power
     
             peak_envelope_power_at_antenna = transmitter_power - power_loss_at_swr
     
